26859986d2067b4f6357c1af4b5c57e95f38b3a6
sreerajpr33 <prsreeraj33@gmail.com>
2024-06-10 09:48:31 +0530

    python

---
+++ conditionalstatements.py
@@ -0,0 +1,6 @@
+year=int(input('enter years of service :'))
+if year>5:
+    salary=int(input('enter current salary :'))
+    print('your new salary is :',salary*0.05+salary)
+else:
+    print('no bonus were added')     
---
+++ task1.py
@@ -0,0 +1,23 @@
+a= 'welcome'
+'''print(a.capitalize())
+print(a.center(20))
+print(a.count('e'))
+print(a.endswith('e'))
+print(a.endswith('w'))
+print(a.find('e'))
+print(a.find('z'))
+print("item name is {} and its price is{}".format('apple',100))
+print(a.index('w')) 
+print(a.index('e'))
+print(a.isdigit())'''
+b='123'
+'''print(b.isdigit())
+print(a.islower())
+print(a.isupper())'''
+c='    welcome      '
+print(c.strip())
+print(c)
+print(c.lstrip())
+print(c.rstrip())
+print(a.upper())
+print(a.lower())
