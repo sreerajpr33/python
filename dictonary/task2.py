2829646e3564dff8fef4861d502aff97a9f8c550
sreerajpr33 <prsreeraj33@gmail.com>
2024-07-01 22:59:29 +0530

    message

--- dictonary/task2.py
+++ dictonary/task2.py
@@ -1,5 +1,20 @@
+# num={0:'zero',1:'one',2:'two',3:'three',4:'four',5:'five',6:'six',7:'seven',8:'eight',9:'nine'}
+# a=int(input("enter the number :"))
+# result = []
+# for digit in str(a):
+#     result.append(num[int(digit)])
+
+# print(" ".join(result))
+
+    
 num={0:'zero',1:'one',2:'two',3:'three',4:'four',5:'five',6:'six',7:'seven',8:'eight',9:'nine'}
 a=int(input("enter the number :"))
-for i in num:
-    
+s= str(a)
+result = ""
+for char in s:
+    digit = int(char)
+    if digit in num:
+        result += num[digit] + " "
+print(result)
+
         
