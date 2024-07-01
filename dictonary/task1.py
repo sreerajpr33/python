94769bbba61562789714814958534ad529acc05f
sreerajpr33 <prsreeraj33@gmail.com>
2024-07-01 11:30:02 +0530

    message

--- dictonary/task1.py
+++ dictonary/task1.py
@@ -1,5 +1,8 @@
 lim=int(input('enter the limit :'))
-dtl=[]
+dtl=[{'name':'a','age':46,'place':'argentina'},
+    {'name':'b','age':20,'place':'paris'},
+    {'name':'c','age':46,'place':'london'},
+    {'name':'d','age':30,'place':'aferica'},]
 for i in range(lim):
     name=input('enter the name :')
     age=int(input('enter the age :'))
@@ -9,3 +12,12 @@ print("{:<10}{:<6}{:<6}".format("name","age","place"))
 print('_'*20)
 for i in dtl:
     print("{:<10}{:<6}{:<6}".format(i['name'],i['age'],i['place']))
+print('age>40')
+for i in dtl:
+    if i['age']>=40:
+        print("{:<10}{:<6}{:<6}".format(i['name'],i['age'],i['place']))  
+
+print('age<40') 
+for i in dtl:
+    if i['age']<=40:
+        print("{:<10}{:<6}{:<6}".format(i['name'],i['age'],i['place'])) 
