46aea06f77df7312ee7fc3b3d38d1aabb3113af1
sreerajpr33 <prsreeraj33@gmail.com>
2024-07-24 14:07:19 +0530

    message

--- database/employe/employe1.py
+++ database/employe/employe1.py
@@ -11,11 +11,22 @@ def add():
 def update():
     oldname=str(input('ENTER THE OLD NAME :'))
     newname=str(input('ENTER NEW NAME'))
-    age =int(input('ENTER THE AGE :'))
-    exp=int(input('EXPERIANCE :'))
-    position=input('ENTER THE POSITION :')
-    salary=int(input('ENTER THE SALARY :'))
-    
+    con.execute("update employe set name=? where name=? ",(newname,oldname))
+    con.commit()
+def delete():
+    dname=str(input('ENTER NAME TO DELETE :'))
+    con.execute("delete from employe where name= ?",(dname,))
+    con.commit()
+def display():
+    data=con.execute("select * from employe")
+    print(data)
 
 
-    
+    print("{:<15}{:<15}{:<15}{:<15}{:<15}".format('name','age','experiance','position','salary'))
+    print('_'*75)
+    for i in data:
+        print("{:<15}{:<15}{:<15}{:<15}{:<15}".format(i[0],i[1],i[2],i[3],i[4]))
+def search():
+    sname=str(input('search the name :'))
+    con.execute("select * from employe where name=?",(sname,))
+
--- database/employe/main.py
+++ database/employe/main.py
@@ -7,9 +7,16 @@ try:
 except:
     pass
 while True:
-    print('1.add\n2.update\n3.delete\ndisplay\n4.search')
+    print('1.add\n2.update\n3.delete\n4.display\n5.search')
     ch=(int(input('enter your choice :')))
     if ch==1:
         add()
-        
+    elif ch==2:
+        update()
+    elif ch==3:
+        delete()  
+    elif ch==4:
+        display()
+    elif ch==5:
+        search()
         
