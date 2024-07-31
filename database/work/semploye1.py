61d0dc87f19828e4b117f1c02fa34c228c7c37f6
sreerajpr33 <prsreeraj33@gmail.com>
2024-07-31 11:26:58 +0530

    message

--- database/work/semploye1.py
+++ database/work/semploye1.py
@@ -12,7 +12,42 @@ cursor = con.cursor()
 
 cursor.execute("CREATE TABLE IF NOT EXISTS emp (emp_id INT, address text,experiance int,position text)")
 
+# data=cursor.fetchall()
+# cursor.execute("select staff.emp_id,staff.name,staff.age,staff.salary,staff.phno,emp.address,emp.experiance,emp.position from staff inner join emp on staff.emp_id=emp.emp_id")
+# data=cursor.fetchall()
+# for i in data:
+#         print(i)
+# print()
 
+# while True:
+#     print("1.add")
+#     choice = int(input("Enter your choice: "))
+#     if choice == 1:
+#         emp_id = int(input('Enter ID: '))
+#         address = input('Enter address: ')
+#         exp = int(input('Enter exp: '))
+#         position = input('Enter pos: ')
+#         # phno = input('Enter phone number: ')  
+#         cursor.execute('insert into emp (emp_id, address, experiance, position) VALUES (%s, %s, %s, %s)', (emp_id, address, exp, position))
+#         con.commit()
+    
+# cursor.execute("select staff.emp_id,staff.name,staff.age,staff.salary,staff.phno,emp.address,emp.experiance,emp.position from staff left join emp on staff.emp_id=emp.emp_id")
+# data=cursor.fetchall()
+# for i in data:
+#         print(i)
+# print()
+
+# cursor.execute("select staff.emp_id,staff.name,staff.age,staff.salary,staff.phno,emp.address,emp.experiance,emp.position from staff right join emp on staff.emp_id=emp.emp_id")
+# data=cursor.fetchall()
+# for i in data:
+#         print(i)
+# print()
+
+# cursor.execute("select staff.emp_id,staff.name,staff.age,staff.salary,staff.phno,emp.address,emp.experiance,emp.position from staff cross join emp")
+# data=cursor.fetchall()
+# for i in data:
+#         print(i)
+# print()
 
 
 
